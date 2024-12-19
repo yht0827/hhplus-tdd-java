d6070596e46146cde219191fa1fd35fca23a9641
yht0827 <yangheetae0827@naver.com>
2024-12-19 23:44:58 +0900

    feat: 동시성 테스트

--- src/main/java/io/hhplus/tdd/point/PointFacade.java
+++ src/main/java/io/hhplus/tdd/point/PointFacade.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.point;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ public class PointFacade {
 	private final PointService pointService;
 	private final PointValidationService pointValidationService;
 	private final PointHistoryService pointHistoryService;
+	private final ReentrantLock lock = new ReentrantLock(true);
 
 	public UserPoint getPointById(long id) {
 		return pointService.getPointById(id);
@@ -23,33 +25,43 @@ public class PointFacade {
 	}
 
 	public UserPoint chargePoint(long id, long amount) {
-		long balance = getBalance(id);
+		lock.lock();
+		try {
+			long balance = getBalance(id);
 
-		// 1. validation
-		pointValidationService.chargePointValidation(balance, amount);
+			// 1. validation
+			pointValidationService.chargePointValidation(balance, amount);
 
-		// 2. save Point
-		UserPoint userPoint = pointService.chargePoint(id, balance, amount);
+			// 2. save Point
+			UserPoint userPoint = pointService.chargePoint(id, balance, amount);
 
-		// 3. save history
-		pointHistoryService.savePointHistory(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+			// 3. save history
+			pointHistoryService.savePointHistory(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
 
-		return userPoint;
+			return userPoint;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	public UserPoint usePoint(long id, long amount) {
-		long balance = getBalance(id);
+		lock.lock();
+		try {
+			long balance = getBalance(id);
 
-		// 1. validation
-		pointValidationService.usePointValidation(balance, amount);
+			// 1. validation
+			pointValidationService.usePointValidation(balance, amount);
 
-		// 2. save Point
-		UserPoint userPoint = pointService.usePoint(id, balance, amount);
+			// 2. save Point
+			UserPoint userPoint = pointService.usePoint(id, balance, amount);
 
-		// 3. save history
-		pointHistoryService.savePointHistory(id, amount, TransactionType.USE, System.currentTimeMillis());
+			// 3. save history
+			pointHistoryService.savePointHistory(id, amount, TransactionType.USE, System.currentTimeMillis());
 
-		return userPoint;
+			return userPoint;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	private long getBalance(long id) {
--- src/test/java/io/hhplus/tdd/InMemoryPointConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/InMemoryPointConcurrencyTest.java
@@ -11,9 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.hhplus.tdd.point.PointService;
+import io.hhplus.tdd.point.PointFacade;
 import io.hhplus.tdd.point.UserPoint;
 
+@DisplayName("포인트 동시성 테스트")
 @SpringBootTest
 public class InMemoryPointConcurrencyTest {
 
@@ -21,20 +22,19 @@ public class InMemoryPointConcurrencyTest {
 	private static final int THREAD_COUNT = 10;
 
 	@Autowired
-	private PointService pointService;
+	private PointFacade pointFacade;
 
 	@DisplayName("동시에 10명 포인트 충전 요청")
 	@Test
 	void chargePointConcurrentTest() throws InterruptedException {
 		long userId = 1L;
-		long amount = 100L;
-
+		long amount = 1000L;
 		CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
+
 		for (int i = 0; i < THREAD_COUNT; i++) {
 			executorService.submit(() -> {
 				try {
-					UserPoint userPoint = pointService.getPointById(userId);
-					pointService.chargePoint(userId, userPoint.point(), amount);
+					pointFacade.chargePoint(userId, amount);
 				} finally {
 					latch.countDown();
 				}
@@ -44,34 +44,63 @@ public class InMemoryPointConcurrencyTest {
 		latch.await();
 		executorService.shutdown();
 
-		UserPoint userPoint = pointService.getPointById(userId);
-		assertThat(userPoint.point()).isEqualTo(1000L);
+		UserPoint userPoint = pointFacade.getPointById(userId);
+		assertThat(userPoint.point()).isEqualTo(10000L);
 	}
 
 	@DisplayName("동시에 10명 포인트 사용")
 	@Test
 	void usePointConcurrentTest() throws InterruptedException {
 		long userId = 1L;
-		long amount = 100L;
+		long amount = 1000L;
 		CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
 
-		pointService.chargePoint(userId, 0L, 1000L);
+		pointFacade.chargePoint(userId, 10000L);
 
 		for (int i = 0; i < THREAD_COUNT; i++) {
 			executorService.submit(() -> {
 				try {
-					UserPoint userPoint = pointService.getPointById(userId);
-					pointService.usePoint(userId, userPoint.point(), amount);
+					pointFacade.usePoint(userId, amount);
 				} finally {
 					latch.countDown();
 				}
 			});
 		}
+
 		latch.await();
 		executorService.shutdown();
 
-		UserPoint userPoint = pointService.getPointById(userId);
+		UserPoint userPoint = pointFacade.getPointById(userId);
 		assertThat(userPoint.point()).isZero();
+	}
 
+	@Test
+	@DisplayName("포인트 충전 및 사용 동시성 테스트")
+	void pointChargeAndUseConcurrencyTest() throws InterruptedException {
+		long userId = 1L;
+		CountDownLatch latch = new CountDownLatch(1);
+
+		executorService.submit(() -> {
+			try {
+				pointFacade.chargePoint(userId, 5000L);
+				pointFacade.usePoint(userId, 3000L);
+				pointFacade.usePoint(userId, 2000L); // 순서대로 처리 하면 0포인트
+				pointFacade.chargePoint(userId, 1000L);
+				pointFacade.chargePoint(userId, 3000L);
+				pointFacade.usePoint(userId, 4000L);
+				pointFacade.chargePoint(userId, 5000L);
+				pointFacade.usePoint(userId, 4000L);
+				pointFacade.chargePoint(userId, 6000L);
+				pointFacade.usePoint(userId, 2000L);
+			} finally {
+				latch.countDown();
+			}
+		});
+
+		latch.await();
+		executorService.shutdown();
+
+		UserPoint userPoint = pointFacade.getPointById(userId);
+		assertThat(userPoint.point()).isEqualTo(5000L);
 	}
 }
